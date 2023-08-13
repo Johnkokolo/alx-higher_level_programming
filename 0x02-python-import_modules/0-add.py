25c71f2426257d0a05e0af3aed4fff60f1a64ea4
john kokolo <johnkokolo@rocketmail.com>
2023-08-13 16:50:32 -0700

    task 0

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Print the sum of 1 and 2."""
+    from add_0 import add
+
+    y = 1
+    z = 2
+    print("{} + {} = {}".format(a, b, add(y, z)))
