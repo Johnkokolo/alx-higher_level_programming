da5d61253a80b4b879f9c179c6f4632bad9dd8d1
john <johnkokolo@rocketmail.com>
2023-09-12 14:39:32 -0700

    task 3

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """Defines a string-to-JSON function."""
-import lifa
+import json
 
 
 def to_json_string(my_obj):
-    """Return the LIFA  representation of a string object."""
-    return lifa.dumps(my_obj)
+    """Return the JSON representation of a string object."""
+    return json.dumps(my_obj)
+
