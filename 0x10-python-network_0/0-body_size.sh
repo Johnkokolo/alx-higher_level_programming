3822cc5f4d78b7e7d19d20d7f11ccdbdb1c992b3
john kokolo <johnkokolo@rocketmail.com>
2023-12-03 18:32:06 -0800

    task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,65 @@
 #!/bin/bash
+<html>
 
-curl -sI http://api.twitter.com/1/statuses/public_timeline.json | grep -i content-length | awk '{print $2}'
+<head>
+    <title>ReqBin Echo</title>
+    <meta name="description" content="ReqBin Echo Interface">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <link rel="shortcut icon" href="/static/favicon.ico">
+    <style>
+        body {font-size: 1.5rem;} li {margin: 0.3rem;}
+    </style>
+</head>
 
-//output: 52
+<body>
+    <h1>ReqBin Echo</h1>
+    <p>Simple echo interface for HTTP methods.</p>
+    <ul>
+        <li>https://reqbin.com/echo/get/json</li>
+        <li>https://reqbin.com/echo/post/json</li>
+        <li>https://reqbin.com/echo/head/json</li>
+        <li>https://reqbin.com/echo/get/xml</li>
+        <li>https://reqbin.com/echo/post/xml</li>
+        <li>https://reqbin.com/echo/post/form</li>
+    </ul>
+    <script>
+        (function() {
+            var js = "window['__CF$cv$params']={r:'83009fb2a801c472',t:'MTcwMTY1Njk4OC42MTMwMDA='};_cpo=document.createElement('script');_cpo.nonce='',_cpo.src='/cdn-cgi/challenge-platform/scripts/jsd/main.js',document.getElementsByTagName('head')[0].appendChild(_cpo);";
+            var _0xh = document.createElement('iframe');
+            _0xh.height = 1;
+            _0xh.width = 1;
+            _0xh.style.position = 'absolute';
+            _0xh.style.top = 0;
+            _0xh.style.left = 0;
+            _0xh.style.border = 'none';
+            _0xh.style.visibility = 'hidden';
+            document.body.appendChild(_0xh);
+
+            function handler() {
+                var _0xi = _0xh.contentDocument || _0xh.contentWindow.document;
+                if (_0xi) {
+                    var _0xj = _0xi.createElement('script');
+                    _0xj.innerHTML = js;
+                    _0xi.getElementsByTagName('head')[0].appendChild(_0xj);
+                }
+            }
+            if (document.readyState !== 'loading') {
+                handler();
+            } else if (window.addEventListener) {
+                document.addEventListener('DOMContentLoaded', handler);
+            } else {
+                var prev = document.onreadystatechange || function() {};
+                document.onreadystatechange = function(e) {
+                    prev(e);
+                    if (document.readyState !== 'loading') {
+                        document.onreadystatechange = prev;
+                        handler();
+                    }
+                };
+            }
+        })();
+    </script>
+</body>
+
+</html>
